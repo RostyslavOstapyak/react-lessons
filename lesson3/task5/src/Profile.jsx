85276350abcebaec66101836c987dbb185f05674
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-30 18:56:45 +0300

    lesson3.5 t5

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -4,10 +4,10 @@ import moment from 'moment';
 const userInfo = (props) =>{
     console.log(props)
     return (
-         <div className="profile">
+    <>
         <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
         <div className="profile__birth">{`Was born ${new Date(props.userData.birthDate).getDate()} ${moment(new Date(props.userData.birthDate)).format("MMM")} ${new Date(props.userData.birthDate).getFullYear()% 100} in ${props.userData.birthPlace}`}</div>
-      </div>
+</>
     )
 }
 
