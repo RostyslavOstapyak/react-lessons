ccc6ecc6b5315cabfc5960d1294b8e1dbb93c6b9
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-30 21:09:14 +0300

    lesson3.5 t10

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = (props) =>{
     return (
     <div className="profile">
         <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-        <div className="profile__birth">{`Was born ${moment(new Date(props.userData.birthDate)).format('DD MMM YY')} 
+        <div className="profile__birth">{`Was born ${moment((props.userData.birthDate)).format('DD MMM YY')} 
         in ${props.userData.birthPlace}`}</div>
     </div>
     );
