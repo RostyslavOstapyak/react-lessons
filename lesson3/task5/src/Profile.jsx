1f2f480e6ff664681d58020c4a9de7f07e4337be
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-09-30 18:47:15 +0300

    lesson3.5 t1

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const userInfo = (props) =>{
+    console.log(props)
     return (
          <div className="profile">
-        <div className="profile__name">{`${props.firstName} ${props.lastName}`}</div>
-        <div className="profile__birth">{`Was born ${new Date(props.birthDate).getDate()} ${new Date(props.birthDate).getMonth()} ${new Date(props.birthDate).getFullYear()% 100} in ${props.birthPlace}`}</div>
+        <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+        <div className="profile__birth">{`Was born ${new Date(props.userData.birthDate).getDate()} ${new Date(props.userData.birthDate).getMonth()} ${new Date(props.userData.birthDate).getFullYear()% 100} in ${props.userData.birthPlace}`}</div>
       </div>
     )
 }
