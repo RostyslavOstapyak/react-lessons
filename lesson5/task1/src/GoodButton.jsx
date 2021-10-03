2d50c012e5d073d900c77e5f7231f4882238f54c
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-03 21:49:49 +0300

    lesson5.1 try1

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GoodButton = () => {
+  return (
+    <button
+      onClick={() => {
+        alert("Good job!");
+      }}
+      className="fancy-button"
+    >
+      Click me!
+    </button>
+  );
+};
+
+export default GoodButton;
