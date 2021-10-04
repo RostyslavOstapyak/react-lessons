02472a0e34bccba90a95f9abc26fc3df7a8942a4
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-05 02:09:57 +0300

    lesson6.2 t1

--- lesson6/task2/src/index.js
+++ lesson6/task2/src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.scss";
-import App from "./App.jsx";
+import Mailbox from "./Mailbox.jsx";
+
+ReactDOM.render(
+  <Mailbox unreadMessages={["a"]} />,
+  document.getElementById("root")
+);
