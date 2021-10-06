6048b163ba43a693381f0f359cc39a0fa82aebd5
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-06 19:29:28 +0300

    lesson7.2 t2

--- lesson7/task2/src/index.js
+++ lesson7/task2/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.scss";
-import UserList from "./UserList.jsx";
+import UserList from "./UsersList.jsx";
 
 const users = [
   {
