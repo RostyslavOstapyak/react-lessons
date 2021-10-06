a2d707ca5a60b8d32555bb9b320590d2b937fc45
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-06 19:27:23 +0300

    lesson7.2 t1

---
+++ lesson7/task2/src/UserList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import User from "./User.jsx";
+
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.users = props.users;
+  }
+  state = { sorting: null };
+
+  toggleSorting = () => {
+    const newSorting = this.state.sorting === "asc" ? "desc" : "asc";
+    this.setState({ sorting: newSorting });
+  };
+
+  render() {
+    let usersList = this.props.users.slice();
+    if (this.state.sorting) {
+      usersList = this.users.sort((a, b) =>
+        this.state.sorting === "asc" ? a.age - b.age : b.age - a.age
+      );
+    }
+
+    return (
+      <div>
+        <button className="btn" onClick={this.toggleSorting}>
+          {this.state.sorting || "-"}
+        </button>
+        <ul className="users">
+          {usersList.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserList;
