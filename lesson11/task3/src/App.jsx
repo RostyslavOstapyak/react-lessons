0ac1101f36c04eec2ae9c3696c5e397e20ec6fa1
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-11 19:24:28 +0300

    lesson 11.3 t4

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import Expand from './Expand.jsx';
 
-class App extends React.Component {
-  state = { contentVisible: false };
-
-  toggleContent = () => {
-    this.setState({ contentVisible: !this.state.contentVisible });
-  };
-
-  render() {
-    return (
-      <Expand
-        toggleContent={this.toggleContent}
-        isVisible={this.state.contentVisible}
-        title="Some title IDK"
-      >
-        idk, some text
-      </Expand>
-    );
-  }
-}
+const App = () => <Expand title="Some title IDK">idk, some text</Expand>;
 
 export default App;
--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-const Expand = ({ children, toggleContent, isVisible, title }) => (
-  <div className="expand border">
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      <button className="expand__toggle-btn" onClick={toggleContent}>
-        {isVisible ? (
-          <i className="fas fa-chevron-up"></i>
-        ) : (
-          <i className="fas fa-chevron-down"></i>
-        )}
-      </button>
-    </div>
-    {isVisible ? <div className="expand__content">{children}</div> : null}
-  </div>
-);
+class Expand extends React.Component {
+  state = { isVisible: false };
 
+  toggleContent = () => {
+    this.setState({ contentVisible: !this.state.contentVisible });
+  };
+
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleContent}>
+            {this.state.isVisible ? (
+              <i className="fas fa-chevron-up"></i>
+            ) : (
+              <i className="fas fa-chevron-down"></i>
+            )}
+          </button>
+        </div>
+        {this.state.isVisible ? <div className="expand__content">{this.props.children}</div> : null}
+      </div>
+    );
+  }
+}
 export default Expand;
