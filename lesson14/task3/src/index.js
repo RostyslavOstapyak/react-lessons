741ad1cb5d7fe98b4793cf5ac713f18f5c84359b
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-14 23:25:47 +0300

    lesson14.3 t1

--- lesson14/task3/src/index.js
+++ lesson14/task3/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.scss";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Demo from './Demo.jsx';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Demo />, document.getElementById('root'));
