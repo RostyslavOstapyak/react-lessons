926f8dfcb20059a4f2f8bc3babe1125a7f0db0c8
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-14 23:43:17 +0300

    lesson 14.4 t2

--- lesson14/task4/src/index.js
+++ lesson14/task4/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.scss";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import ConnectionStatus from './ConnectionStatus.jsx';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ConnectionStatus />, document.getElementById('root'));
