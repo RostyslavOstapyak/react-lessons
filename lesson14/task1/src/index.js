5a20db0065e1190d27fe77bf39dc1a79b8960466
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-14 19:54:39 +0300

    lesson14.1 t1

--- lesson14/task1/src/index.js
+++ lesson14/task1/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.scss";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Counter from './Counter.jsx';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById('root'));
